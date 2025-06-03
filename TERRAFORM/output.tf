c0b7acb68d7be440ae16917bceadd0beb0060b7d
Daniel Calatayud <danycg@hotmail.com>
2025-06-03 13:01:59 +0200

    Update output.tf

--- TERRAFORM/output.tf
+++ TERRAFORM/output.tf
@@ -1,1 +1,19 @@
+output "mentoria_vpc" {
+  description = "ID de la VPC creada"
+  value       = aws_vpc.mentoria_vpc.id
+}
 
+output "public_subnet" {
+  description = "ID de la subred creada"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "windows_dc_publica" {
+  description = "Dirección IP pública de la instancia EC2"
+  value       = aws_instance.windows_dc.public_ip
+}
+
+output "windows_dc_privada" {
+  description = "Dirección IP privada de la instancia EC2"
+  value       = aws_instance.windows_dc.private_ip
+}
